2d2b55e5bb66ab16f8c6752b0588aeb5df38f8fa
AFEKl <vladilavniy@gmail.com>
2024-11-29 20:17:00 +0700

    Signup/Signin

--- core/views.py
+++ core/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
+def signin(request):
+    return render(request, "core/sign-in.html")
+
+def signup(request):
+    return render(request, "core/sign-up.html")
+
+def profile(request):
+    profile = User.objects.all()
+    return render(request, "core/profile.html", {"profile":profile})
