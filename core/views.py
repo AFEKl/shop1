51c19f62b59097080c38efcaea8f9edc3bce224a
AFEKl <vladilavniy@gmail.com>
2024-12-06 20:22:34 +0700

    sign_up

--- core/views.py
+++ core/views.py
@@ -6,8 +6,15 @@ def signin(request):
     return render(request, "core/sign-in.html")
 
 def signup(request):
+    if request.method == "POST":
+        u_name = request.POST["username"]
+        u_password = request.POST["password"]
+        u_email = request.POST["email"]
+        u_repassword = request.POST["repeat_password"]
+        if u_password != u_repassword:
+            return render(request, "core/sign-up.html",{'error':"пароли не совпадают"})
     return render(request, "core/sign-up.html")
 
 def profile(request):
-    profile = User.objects.all()
-    return render(request, "core/profile.html", {"profile":profile})
+
+    return render(request, "core/profile.html")
