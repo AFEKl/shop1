0af4ec3f70efb91fcfeb82841f85109cd919f051
AFEKl <vladilavniy@gmail.com>
2024-12-14 23:40:48 +0700

    login error

--- shop/views.py
+++ shop/views.py
@@ -14,20 +14,23 @@ def orders(request):
 def create(request, product_id):
     product = Product.objects.get(id=product_id)
     if request.method == "POST":
-        u_address = request.POST["address"]
-        u_email = request.POST["email"]
-        u_phone = request.POST["phone"]
-        u_count = int(request.POST["count"])
-        u_comment = request.POST["comment"]
-        Order.objects.create(
-            product = product,
-            address = u_address,
-            phone = u_phone,
-            email = u_email,
-            comment = u_comment,
-            count = u_count
-        )
-    return render(request, "shop/create_order.html")
+        if request.user.is_authenticated:
+            u_address = request.POST["address"]
+            u_email = request.POST["email"]
+            u_phone = request.POST["phone"]
+            u_count = int(request.POST["count"])
+            u_comment = request.POST["comment"]
+            Order.objects.create(
+                product = product,
+                address = u_address,
+                phone = u_phone,
+                email = u_email,
+                comment = u_comment,
+                count = u_count
+            )
+        else:
+            return render(request, "shop/create_order.html",{'error':"Вы не авторизованы! Пожалуйста, авторизуйтесь."})
+    return render(request, "shop/create_order.html", {"success": "Заказ успешно создан!"})
 
 
 def detail(request, product_id):
